d61e68831572630f3eece75587ec2cfea125d530
xxAmnesiAxx <xtrollamnesiax@gmail.com>
2022-10-16 05:14:29 +0000

    Deploying to gh-pages from @ xxAmnesiAxx/itcats@1648bd8422236d2a30aa82599958beef4d961875 🚀

---
+++ index.d6e29367.js
@@ -0,0 +1,2 @@
+$(document).ready((function(){$(".slick-slider").slick({dots:!0,slidesToShow:1,slidesToScroll:1,autoplay:!0,autoplaySpeed:3e3,adaptiveHeight:!0})}));
+//# sourceMappingURL=index.d6e29367.js.map
