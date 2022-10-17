f7c88596e86bbfdde4328c66927c8e78ab7d3316
xxAmnesiAxx <xtrollamnesiax@gmail.com>
2022-10-17 12:47:15 +0000

    Deploying to gh-pages from @ xxAmnesiAxx/itcats@db2371353078e6ac4b082dcf662a48aea0189298 🚀

---
+++ index.4438f9e8.js
@@ -0,0 +1,2 @@
+!function(){var e={openModalBtn:document.querySelector("[modal-open]"),closeModalBtn:document.querySelector("[modal-close]"),modal:document.querySelector("[modal]")};function o(){e.modal.classList.toggle("is-hidden")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)}();
+//# sourceMappingURL=index.4438f9e8.js.map
