cc362276b6011556aeea2786f89bb71ba86027d1
SergeyMark <playsforfuns@gmail.com>
2022-10-18 18:37:22 +0300

    btn up done

---
+++ src/js/btn-scrol.js
@@ -0,0 +1,18 @@
+function backToTop() {
+    let button = $('.btn-up');
+
+    $(window).on('scroll', () => {
+        if ($(this).scrollTop() >= 50) {
+            button.fadeIn();
+        } else {
+            button.fadeOut();
+        }
+    });
+
+    button.on('click', (e) => {
+        e.preventDefault();
+        $('html').animate({ scrollTop: 0 }, 250);
+    })
+}
+
+backToTop();
