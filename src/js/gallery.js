52b202145bae49e41805bd7f8f2dc6c78e237da7
SergeyMark <playsforfuns@gmail.com>
2022-10-18 09:33:41 +0300

    фиксанул галерею

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -1,9 +1,13 @@
 $(document).ready(function(){
     $('.slick-slider-gallery').slick({
         slidesToShow: 1,
-        slidesToScroll: 1,
+        // slidesToScroll: 1,
         autoplay: true,
         autoplaySpeed: 4000,
         adaptiveHeight: true,
+        infinite: true,
+        speed: 1000,
+        fade: true,
+        cssEase: 'linear'
   });
 });
