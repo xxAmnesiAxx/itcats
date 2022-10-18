39f82df280d21e3dc6e0ce88ba1562c8fd0a3a43
sergii-borodin <sergii.borodin@yahoo.com>
2022-10-18 07:42:20 +0000

    Deploying to gh-pages from @ xxAmnesiAxx/itcats@3a76142a5a5320dd2eb71fa013e67b1fed8028b0 🚀

--- index.0ff0f735.js
+++
@@ -1,2 +0,0 @@
-$(document).ready((function(){$(".slick-slider-gallery").slick({slidesToShow:1,slidesToScroll:1,autoplay:!0,autoplaySpeed:4e3,adaptiveHeight:!0})}));
-//# sourceMappingURL=index.0ff0f735.js.map
---
+++ index.7ac61954.js
@@ -0,0 +1,2 @@
+$(document).ready((function(){$(".slick-slider-gallery").slick({slidesToShow:1,autoplay:!0,autoplaySpeed:4e3,adaptiveHeight:!0,infinite:!0,speed:1e3,fade:!0,cssEase:"linear"})}));
+//# sourceMappingURL=index.7ac61954.js.map
