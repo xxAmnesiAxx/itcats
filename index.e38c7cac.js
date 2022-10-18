801d885a1fad7dea6517b041c8c8eaaf5d0de2f5
sergii-borodin <sergii.borodin@yahoo.com>
2022-10-18 08:33:32 +0000

    Deploying to gh-pages from @ xxAmnesiAxx/itcats@52694b160767bdf8153d15c48b1f1f1f70852a24 🚀

---
+++ index.e38c7cac.js
@@ -0,0 +1,2 @@
+$(document).ready((function(){$(".slick-slider-gallery").slick({slidesToShow:1,autoplay:!0,autoplaySpeed:4e3,adaptiveHeight:!0,infinite:!0,speed:500,fade:!0,cssEase:"linear"})}));
+//# sourceMappingURL=index.e38c7cac.js.map
