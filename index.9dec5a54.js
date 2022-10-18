3f039bbe3ba5eeb0d4bb91fea4cbad64a9f6e58c
xxAmnesiAxx <xtrollamnesiax@gmail.com>
2022-10-18 15:42:25 +0000

    Deploying to gh-pages from @ xxAmnesiAxx/itcats@2a4860e622fe303accbe8ac184e89efff163d024 🚀

---
+++ index.9dec5a54.js
@@ -0,0 +1,2 @@
+function backToTop(){var o=this,n=$(".btn-up");$(window).on("scroll",(function(){$(o).scrollTop()>=50?n.fadeIn():n.fadeOut()})),n.on("click",(function(o){o.preventDefault(),$("html").animate({scrollTop:0},250)}))}backToTop();
+//# sourceMappingURL=index.9dec5a54.js.map
