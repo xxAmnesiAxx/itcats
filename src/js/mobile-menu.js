dd38831e47e341f73d697ab4b84968b8073560eb
BoroDinS <sergey.borodin.lg@gmail.com>
2022-10-19 22:10:35 +0200

    fix disabled scroll after using a navigation link on mobile menu

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -1,35 +1,40 @@
 (() => {
-  const mobileMenu = document.querySelector('.js-menu-container');
-      const menuLink = document.querySelectorAll(".mobile-nav__link");   
-  const openMenuBtn = document.querySelector('.js-open-menu');
-  const closeMenuBtn = document.querySelector('.js-close-menu');
-
-  const toggleMenu = () => {
-    const isMenuOpen =
-      openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
-    openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
-    mobileMenu.classList.toggle('is-open');
-
-    const scrollLockMethod = !isMenuOpen
-      ? 'disableBodyScroll'
-      : 'enableBodyScroll';
-    bodyScrollLock[scrollLockMethod](document.body);
-  };
-
-  openMenuBtn.addEventListener('click', toggleMenu);
-  closeMenuBtn.addEventListener('click', toggleMenu);
-
-  // Close the mobile menu on wider screens if the device orientation changes
+    const mobileMenu = document.querySelector(".js-menu-container");
+    const menuLink = document.querySelectorAll(".mobile-nav__link");   
+    const menuBtnOpen = document.querySelector(".js-open-menu");
+    const menuBtnClose = document.querySelector('.js-close-menu');
+  const body = document.body;
+  
+  // Open the mobile menu by click on the menuBtnOpen
+    menuBtnOpen.addEventListener("click", () => {
+        // console.log(mobileMenu);
+        const expanded = menuBtnOpen.getAttribute("aria-expanded") === "true" || false;
+        mobileMenu.classList.toggle("is-open");
+        menuBtnOpen.setAttribute("aria-expanded", !expanded);
+        body.classList.add('overflow');
+    });
+  
+    // Close the mobile menu by click on the menuBtnClose
+    menuBtnClose.addEventListener('click', () => {
+        const expanded = menuBtnClose.getAttribute('aria-expanded') === 'false' || true;
+        menuBtnOpen.setAttribute('aria-expanded', !expanded);
+        mobileMenu.classList.toggle('is-open');
+        body.classList.remove('overflow');
+    });
+  
+    // Close the mobile menu on wider screens if the device orientation changes
   window.matchMedia('(min-width: 1200px)').addEventListener('change', e => {
     if (!e.matches) return;
     mobileMenu.classList.remove('is-open');
     openMenuBtn.setAttribute('aria-expanded', false);
-    bodyScrollLock.enableBodyScroll(document.body);
+    body.classList.remove('overflow');
   });
-  
-   menuLink.forEach(item => {
+
+   // Close the mobile menu by click on a navigation link in the mobile menu
+    menuLink.forEach(item => {
         item.addEventListener('click', function () {
-            mobileMenu.classList.remove('is-open');
+            mobileMenu.classList.toggle('is-open');
+            body.classList.remove('overflow');
         });
     });
 })();
